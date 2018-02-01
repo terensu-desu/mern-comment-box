2811d2540d5695a05d8a6983e82ddddbe04dc024
Terence Mangram <tmangram@gmail.com>
2018-02-01 20:28:40 +0900

    Remove unnecessary text

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentBox from "./CommentBox";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<CommentBox url="http://localhost:3001/api/comments" pollInterval={2000} />,
+	document.getElementById("root")
+);
